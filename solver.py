2b114a0d9acaec1134e6560a8d5bb1e298533357
suneet <suneetpathangay@gmail.com>
2023-08-17 14:24:41 -0700

    added user input

--- solver.py
+++ solver.py
@@ -1,12 +1,32 @@
 
 ##using ocr we can capture the words in the text and add them to a list
-words_text=["TAB","TEA","BAT","BEAT","ATE","BET","BATE"]
+
+##testing case
+
+# words_text=["TAB","TEA","BAT","BEAT","ATE","BET","BATE"]
 
 
-list_letters=["A", "B", "T" ,"E"]
+# list_letters=["A", "B", "T" ,"E"]
 
 ##implementing the backtracking algorithm
+
+def get_words():
+    with open('1-1000.txt') as file:
+        words=file.readlines()
+        def remove(word):
+            return word.strip()
+        words=list(map(remove,words))
+        return words
+
+
+inp = input('Enter in your letters\n')
+
+list_letters=list(inp)
+words_text=get_words()
+
 solution=[]
+
+
 
 def get_chars(input):
     curr=""
@@ -17,7 +37,7 @@ def get_chars(input):
 
 
 def backtrack(curr,length):
-    word=get_chars(curr)
+    word=get_chars(curr).lower()
     
     if len(word)==length and word in words_text:
         solution.append(word)
